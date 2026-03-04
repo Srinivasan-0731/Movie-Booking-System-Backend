5941d4138a6226b5140b985821f00222707d29cf
Srinivasan-0731 <sp2392546@gmail.com>
2026-03-04 12:50:48 +0530

    initial commit

---
+++ server/controllers/bookingController.js
@@ -0,0 +1,105 @@
+import Booking from "../models/Booking.js";
+import Show from "../models/show.js"
+import razorpay from "razorpay"
+
+
+//Function to check availability of selected seats for a movie
+const checkSeatsAvailability = async (showId, selectedSeats) => {
+    try {
+        await Show.findById(showId)
+        if(!showData) return false;
+
+        const occupiedSeats = showData.occupiedSeats;
+
+        const isAnySeatTaken = selectedSeats.some(seat => occupiedSeats[seat]);
+
+        return !isAnySeatTaken;
+    } catch (error) {
+        console.log(error.message);
+        return false;
+    }
+}
+
+export const createBooking = async (req, res) => {
+    try {
+        const {userId} = req.auth();
+        const {showId, selectedSeats} = req.body;
+        const { origin } = req.headers;
+
+        // Check if the seat is available for the selected show
+        const isAvailable = await checkSeatsAvailability(showId, selectedSeats)
+
+        if(!isAvailable){
+            return res.json(({success: false, message: "Selected Seats are not available."}))
+        }
+
+        // Get the show details
+        const showData = await Show.findById(showId).populate('movie');
+
+        // Create a new booking
+        const booking = await Booking.create({
+            user: userId,
+            show: showId,
+            amount: showData.showPrice * selectedSeats.length,
+            bookegSeats: selectedSeats
+        })
+
+        selectedSeats.map((seat) => {
+            showData.occupiedSeats[seat] = userId;
+        })
+
+        showData.markModified('occupiedSeats');
+
+        await showData.save();
+
+        // Razorpay Gateway Initialize
+        const razorpayInstance = new razorpay(process.env.RAZORPAY_SECRET_KEY)
+
+        // Creating line items to for Razorpay
+        const line_items = [{
+            price_data: {
+                currency: 'inr',
+                product_data:{
+                    name: showData.movie.title
+                },
+                unit_amount: Math.floor(booking.amount) * 100
+            },
+            quantity: 1
+        }]
+
+        const session = await razorpayInstance.checkout.session.create({
+            success_url: `${origin}/loading/my-bookings`,
+            cancel_url: `${origin}/my-bookings`,
+            line_items: line_items,
+            mode: 'payment',
+            metaData: {
+                bookingId: booking._id.toString()
+            },
+            expires_at: Math.floor(Date.now() / 1000) + 30 * 60, //Expires in 30 mintues
+        })
+
+        booking.paymentLink = session.cancel_url 
+        await booking.save()
+        
+
+        res.json({success: true, url:session.url})
+    } catch (error) {
+        console.log(error.meesage);
+        res.json({success: false, message: error.message})
+
+    }
+}
+
+export const getOccupiedSeats = async (req, res) => {
+    try {
+        const {showId} = req.params;
+        const showData = await Show.findById(showId)
+
+        const occupiedSeats = Object.keys(showData.occupiedSeats)
+
+        res.json({success: true, occupiedSeats})
+    } catch (error) {
+        console.log(error.meesage);
+        res.json({success: false, message: error.message})
+    }
+}
